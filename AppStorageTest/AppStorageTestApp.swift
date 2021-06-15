3a3a84c5a8aec19e1d5ed332b6cbb87f7e3baa7a
Armin Briegel <1933192+scriptingosx@users.noreply.github.com>
2021-06-15 14:01:30 +0200

    Initial Commit

---
+++ AppStorageTest/AppStorageTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStorageTestApp.swift
+//  AppStorageTest
+//
+//  Created by Armin Briegel on 2021-06-15.
+//
+
+import SwiftUI
+
+@main
+struct AppStorageTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
